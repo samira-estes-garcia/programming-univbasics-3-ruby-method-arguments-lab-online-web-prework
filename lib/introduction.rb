9cc6b1dc768ce2b94d53c00652df13c5adea3b2f
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-08-29 17:45:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,13 @@ end
 
 introduction(name)
 
+def introduction_with_language(name, language) 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning program in #{language}"
+end
   
 
 
